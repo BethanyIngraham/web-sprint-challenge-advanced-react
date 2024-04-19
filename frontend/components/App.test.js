3cf6349fb0739d56be14ed6af38aeeefb4e4d691
Bethany Ingraham <BethFIngraham@gmail.com>
2024-04-19 00:52:42 -0400

    tests refigured

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -5,37 +5,30 @@ import userEvent from "@testing-library/user-event";
 import AppFunctional from "./AppFunctional";
 
 
-let coordinates, steps, message, submit;
-const setElements = (document) => {
-  coordinates = document.querySelector('#coordinates');
-  steps = document.querySelector('#steps');
-  message = document.querySelector('#message');
-  submit = document.querySelector('#submit');
-};
-
 // Write your tests here
 describe("function test", () => {
   beforeEach(() => {
     render(<AppFunctional />);
-    setElements(document);
   });
   test("text for up directional button is rendered on the screen", () => {
     const text = screen.getByText("UP");
     expect(text).toBeVisible();
   });
   test("the coordinates message is rendered on the screen", () => {
+    const coordinates = screen.getByText("Coordinates (2,2)");
     expect(coordinates).toBeVisible();
-    expect(coordinates).toHaveTextContent("Coordinates (2,2)");
   });
   test("the steps message is rendered on the screen", () => {
+    const steps = screen.getByText("You moved 0 times");
     expect(steps).toBeVisible();
-    expect(steps).toHaveTextContent("You moved 0 times");
   });
   test("the form submit message renders on the screen", async () => {
     const user = userEvent.setup();
     const input = screen.getByPlaceholderText("type email");
+    const submit = screen.getByRole("button", {name: "Submit"});
     await user.type(input, "hello@gmail.com");
     await user.click(submit);
+    const message = await screen.findByText("hello win #27");
     expect(message).toBeVisible();
   });
   test("typing on the input results in its value changing to the entered text", async () => {
